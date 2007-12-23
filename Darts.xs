75876a7f01fa2c9c9c60d323f55a7e9e774161ff
dankogai <dankogai@d0d07461-0603-4401-acd4-de1884942a52>
2007-12-23 15:29:44 +0000

    share/lang/perl/Text-Darts Text::Darts - Perl interface to DARTS by Taku Kudoh
    git-svn-id: http://svn.coderepos.org/share/lang/perl/Text-Darts/trunk@3500 d0d07461-0603-4401-acd4-de1884942a52

---
+++ Darts.xs
@@ -0,0 +1,126 @@
+#ifdef __cplusplus
+extern "C" {
+#endif
+#include "EXTERN.h"
+#include "perl.h"
+#include "XSUB.h"
+#ifdef __cplusplus
+}
+#endif
+
+#include <darts.h>
+#define MAX_NMATCH 1024
+
+int da_make(AV *av){
+    Darts::DoubleArray *dp = new Darts::DoubleArray;
+    std::vector <Darts::DoubleArray::key_type *> keys;
+    int i, l;
+    for (i = 0, l = av_len(av)+1; i < l; i++){
+	keys.push_back(SvPV_nolen(AvARRAY(av)[i]));
+    }
+    dp->build(keys.size(), &keys[0], 0, 0);
+    return PTR2IV(dp);
+}
+
+int da_free(int dpi){
+    delete INT2PTR(Darts::DoubleArray *, dpi);
+}
+
+int da_search(int dpi, char *str){
+    Darts::DoubleArray *dp = INT2PTR(Darts::DoubleArray *, dpi);
+    Darts::DoubleArray::result_pair_type  result_pair [MAX_NMATCH];
+    size_t num = dp->commonPrefixSearch(str, result_pair, sizeof(result_pair));
+    return num;
+}
+
+
+static SV *
+do_callback(SV *callback, SV *s){
+    dSP;
+    int argc;
+    SV *retval;
+    ENTER;
+    SAVETMPS;
+    PUSHMARK(sp);
+    XPUSHs(s);
+    PUTBACK;
+    argc = call_sv(callback, G_SCALAR);
+    SPAGAIN;
+    if (argc != 1){
+        croak("fallback sub must return scalar!");
+    }
+    retval = newSVsv(POPs);
+    PUTBACK;
+    FREETMPS;
+    LEAVE;
+    return retval;
+}
+
+SV *da_gsub(int dpi, SV *src, SV *callback){
+    SV *result = newSV(0);
+    Darts::DoubleArray *dp = INT2PTR(Darts::DoubleArray *, dpi);
+    Darts::DoubleArray::result_pair_type  result_pair[MAX_NMATCH];
+
+    char *head = SvPV_nolen(src);
+    char *tail = head + SvCUR(src);
+
+    while (head < tail) {
+	size_t size = 
+	    dp->commonPrefixSearch(head,result_pair, sizeof(result_pair));
+	size_t seekto = 0;       
+	if (size) {
+	    for (size_t i = 0; i < size; ++i) {
+		if (seekto < result_pair[i].length)
+		    seekto = result_pair[i].length;
+	    }
+	    if (seekto) {
+		SV *ret = do_callback(callback, newSVpvn(head, seekto));
+		sv_catsv(result, ret);
+		head += seekto;
+	    }
+	}
+	if (seekto == 0) {
+	    sv_catpvn(result, head, 1);
+	    ++head; 
+	}
+    }
+    return result;
+}
+
+MODULE = Text::Darts		PACKAGE = Text::Darts		
+
+int
+xs_make(av)
+   AV *av
+CODE:
+   RETVAL = da_make(av);
+OUTPUT:
+   RETVAL
+
+int
+xs_free(dpi)
+    int  dpi;
+CODE:
+    RETVAL = da_free(dpi);
+OUTPUT:
+    RETVAL
+
+
+SV *
+xs_gsub(dpi, src, callback)
+   int dpi;
+   SV *src;
+   SV *callback; 
+CODE:
+   RETVAL = da_gsub(dpi, src, callback);
+OUTPUT:
+   RETVAL
+
+int
+xs_search(dpi, str)
+    int  dpi;
+    char *str;
+CODE:
+    RETVAL = da_search(dpi, str);
+OUTPUT:
+    RETVAL
---
+++ lib/Text/Darts.pm
@@ -0,0 +1,115 @@
+package Text::Darts;
+use strict;
+use warnings;
+use Carp;
+our $VERSION = sprintf "%d.%02d", q$Revision: 0.1 $ =~ /(\d+)/g;
+our $DEBUG = 0;
+
+require XSLoader;
+XSLoader::load('Text::Darts', $VERSION);
+
+sub new{
+    my $pkg = shift;
+    my $dpi = xs_make([sort @_]);
+    bless \$dpi, $pkg;
+}
+
+sub DESTROY{
+    if ($DEBUG){
+	no warnings 'once';
+	require Data::Dumper;
+	local $Data::Dumper::Terse  = 1;
+	local $Data::Dumper::Indent = 0;
+	warn "DESTROY:", Data::Dumper::Dumper($_[0]);
+    }
+    xs_free(${$_[0]});
+}
+
+sub search{
+    xs_search(${$_[0]}, $_[1]);
+}
+
+sub gsub{
+    my $cbstr = $_[2];
+    no warnings 'uninitialized';
+    my $cb = ref $cbstr ? $cbstr : sub { $cbstr };
+    xs_gsub(${$_[0]}, $_[1], $cb);
+}
+
+
+if ($0 eq __FILE__){
+    sub say { print @_, "\n" };
+    my @a = ("ALGOL", "ANSI", "ARCO",  "ARPA", "ARPANET", "ASCII");
+    my $da = __PACKAGE__->new(@a);
+    say $da->gsub("I don't like ALGOL at all!", sub{"<<$_[0]>>"});
+    say $da->gsub("I don't like nomatch at all!");
+}
+
+1;
+__END__
+# Below is stub documentation for your module. You'd better edit it!
+
+=head1 NAME
+
+Text::Darts - Perl interface to DARTS by Taku Kudoh
+
+=head1 SYNOPSIS
+
+  use Text::Darts; 
+  my $td     = Text::Darts->new(qw/ALGOL ANSI ARCO ARPA ARPANET ASCII/);
+  my $newstr = $td->gsub("ARPANET is a net by ARPA", sub{ "<<$_[0]>>" });
+  $newstr is now "<<ARPANET>> is a net by <<ARPA>>".
+
+=head1 DESCRIPTION
+
+Darts, or Double-ARray Trie System is a C++ Template Library by Taku Kudoh.
+This module makes use of Darts to implement global replace like below;
+
+  $str = s{ (foo|bar|baz) }{ "<<$1>>" }msgex;
+
+The problem with regexp is that it is slow with alterations.  Suppose
+you want to anchor all words that appear in /usr/share/dict/words with
+regexp.  It would be impractical with regexp but Darts make it
+practical.
+
+=head1 REQUIREMENT
+
+Darts 0.31 or above.  Available at 
+
+L<http://chasen.org/~taku/software/darts/index.html> (Japanese)
+
+L<http://chasen.org/~taku/software/darts/src/darts-0.31.tar.gz>
+
+To install, just
+
+  fetch http://chasen.org/~taku/software/darts/src/darts-0.31.tar.gz
+  tar zxvf darts-0.31.tar.gz
+  cd darts-0.31
+  configure
+  make
+  make check
+  sudo make install
+
+=head2 EXPORT
+
+None.
+
+=head1 SEE ALSO
+
+L<http://chasen.org/~taku/software/darts/index.html> (Japanese)
+
+L<Regexp::Assemble>
+
+=head1 AUTHOR
+
+Dan Kogai, E<lt>dankogai@dan.co.jpE<gt>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2007 by Dan Kogai
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself, either Perl version 5.10.0 or,
+at your option, any later version of Perl 5 you may have available.
+
+=cut
